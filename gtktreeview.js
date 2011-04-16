1046ed500e1775dc48430e0d228fb0cc8b8fc410
Alan Knowles <alan@akbkhome.com>
2011-04-16 12:08:18 +0800

    update code to match what introspection currently does

--- gtktreeview.js
+++ gtktreeview.js
@@ -18,15 +18,14 @@ v.append_column(c);
 var m = new Gtk.ListStore();
 m.set_column_types(1, [GObject.TYPE_INT]);
 
-var iter = new Gtk.TreeIter();
-m.append(iter);
-m.set_value(iter, 0, [GObject.TYPE_INT, 3]);
-m.append(iter);
-m.set_value(iter, 0, [GObject.TYPE_INT, 2]);
-m.append(iter);
-m.set_value(iter, 0, [GObject.TYPE_INT, 9]);
+var r = {};
+m.append(r);
+m.set_value(r.iter, 0, [GObject.TYPE_INT, 3]);
+m.append(r);
+m.set_value(r.iter, 0, [GObject.TYPE_INT, 2]);
+m.append(r);
+m.set_value(r.iter, 0, [GObject.TYPE_INT, 9]);
 
-//TODO: FIXME: Why does property not work here?
 v.set_model(m);
 w.add(v);
 
